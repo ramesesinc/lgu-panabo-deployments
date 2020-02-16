870daef3dc846e2b4d29e589297ff21ace86b217
worgflores <worgflores@gmail.com>
2020-02-16 22:35:59 +0800

    _res: added new patch

---
+++ _res/patches/v2.5.5_20200216_2222.patch.market/mysql/03-market_v254_treasury.sql
@@ -0,0 +1,3 @@
+
+INSERT IGNORE INTO `collectiontype` (`objid`, `state`, `name`, `title`, `formno`, `handler`, `allowbatch`, `barcodekey`, `allowonline`, `allowoffline`, `sortorder`, `org_objid`, `org_name`, `fund_objid`, `fund_title`, `category`) 
+VALUES ('MARKET_RENTAL', 'DRAFT', 'MARKET_RENTAL', 'MARKET RENTAL', '51', 'market', NULL, NULL, '1', '1', '0', NULL, NULL, NULL, NULL, NULL);
