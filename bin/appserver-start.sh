a1403bf3b86fa71d0e6aa591f8fceb0cb4d6508d
worgflores <worgflores@gmail.com>
2021-02-19 20:53:50 +0800

    updated

--- bin/appserver-start.sh
+++ bin/appserver-start.sh
@@ -7,6 +7,6 @@ cd $BASE_DIR/appserver/epayment && docker-compose up -d
 
 cd $BASE_DIR/appserver/etracs && docker-compose up -d
 
-cd $BASE_DIR/appserver/market && docker-compose up -d
+##cd $BASE_DIR/appserver/market && docker-compose up -d
 
 cd $RUN_DIR
--- bin/appserver-stop.sh
+++ bin/appserver-stop.sh
@@ -8,6 +8,6 @@ cd $BASE_DIR/appserver/epayment && docker-compose down
 
 cd $BASE_DIR/appserver/etracs && docker-compose down
 
-cd $BASE_DIR/appserver/market && docker-compose down
+##cd $BASE_DIR/appserver/market && docker-compose down
 
 cd $RUN_DIR
